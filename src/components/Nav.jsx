7db571f10546c2b15a38faf9bea8385fccc09a4f
gamadjg <gamadavid36@gmail.com>
2024-01-19 02:30:23 -0800

    Updated font for nav fields

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ export const Nav = (props) => {
 	const { contentAreas } = props;
 	return (
 		<div className='nav'>
-			<img alt='logo'></img>
+			<div>LOGO{/* <img alt='logo'></img> */}</div>
 			<ToggleField toggleFieldList={contentAreas} toggleClass='nav-toggle' />
 		</div>
 	);
